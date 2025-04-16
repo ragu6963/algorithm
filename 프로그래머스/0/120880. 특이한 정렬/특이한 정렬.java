827bc7349e66beb326c70e43b04be9b787846c3d
Beemo <32388270+ragu6963@users.noreply.github.com>
2025-04-16 10:12:47 +0900

    [level 0] Title: 특이한 정렬, Time: 1.21 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -1,23 +1,27 @@
 import java.util.*;
+
 class Solution {
-    public int[] solution(int[] numlist, int n) {
-        int[] answer = new int[numlist.length];
-        List<Integer> list = new ArrayList<>();
-        for(int number: numlist) {
-            list.add(number);
-        }
-        list.sort((a,b) -> {
-            if (Math.abs(n-a) != Math.abs(n-b)) {
-                return Math.abs(n-a) - Math.abs(n-b);
-            } else {
-                return b - a;
-            }
-            
-        });
+	public int[] solution(int[] numlist, int n) {
+		int[] answer = new int[numlist.length];
         
-        for(int i = 0; i < list.size(); i++){
-            answer[i] = list.get(i);
-        }
-        return answer;
-    }
+		List<Integer> list = new ArrayList<>();
+        
+		for (int number : numlist) {
+			list.add(number);
+		}
+        
+		list.sort((a, b) -> {
+			if (Math.abs(n - a) != Math.abs(n - b)) {
+				return Math.abs(n - a) - Math.abs(n - b);
+			} else {
+				return b - a;
+			}
+
+		});
+
+		for (int i = 0; i < list.size(); i++) {
+			answer[i] = list.get(i);
+		}
+		return answer;
+	}
 }
