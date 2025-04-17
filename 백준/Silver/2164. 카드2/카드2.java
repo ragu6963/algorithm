1de1515d1c769d3bcc32869c79928b46b5ffcd8c
Beemo <32388270+ragu6963@users.noreply.github.com>
2025-04-17 14:12:39 +0900

    [Silver IV] Title: 카드2, Time: 156 ms, Memory: 45668 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,24 +1,24 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		Deque<Integer> deque = new ArrayDeque<>();
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            q.offer(i);          // 카드 1 ~ N 삽입
+        }
 
-		while (N > 0) {
-			deque.push(N);
-			N -= 1;
-		}
+        while (q.size() > 1) {
+            q.poll();            // ① 맨 앞 카드 버리기
+            q.offer(q.poll());   // ② 다음 카드를 맨 뒤로 이동
+        }
 
-		while (deque.size() != 1) {
-			deque.pop();
-			int top = deque.pop();
-			deque.add(top);
-		}
-		System.out.println(deque.pop());
-
-	}
+        System.out.println(q.peek()); // 마지막 카드 출력
+    }
 }
